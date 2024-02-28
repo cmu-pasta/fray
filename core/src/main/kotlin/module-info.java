304866600646cd9617fc36e59aff5a4bcbce709d
Ao Li <leeleo3x@gmail.com>
2024-02-28 00:57:52 -0500

    cleanup.

--- core/src/main/kotlin/module-info.java
+++
@@ -1,6 +0,0 @@
-module sfuzz.core.main {
-    requires annotations;
-    requires kotlin.stdlib;
-    requires cmu.pasta.sfuzz.instrumentation;
-    requires cmu.pasta.sfuzz.runtime;
-}
--- examples/src/main/java/module-info.java
+++
@@ -1,2 +0,0 @@
-module sfuzz.examples.main {
-}
