1364725ce8c3de1b3b53832bd5cf19ac4ea36d3e
Ao Li <leeleo3x@gmail.com>
2024-03-14 14:51:40 -0400

    update.

--- integration-tests/src/test/java/cmu/pasta/sfuzz/it/AtomicTest.java
+++ integration-tests/src/test/java/cmu/pasta/sfuzz/it/AtomicTest.java
@@ -4,19 +4,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AtomicTest extends IntegrationTestRunner {
-    @Test
-    public void testInterleaving1() {
-        runTest("T1T2");
-    }
-
-    @Test
-    public void testInterleaving2() {
-        runTest("T2T1");
-    }
-
-    @Test
-    public void testInterleaving3() {
-        runTest("alternateT2T1");
-    }
-}
+//public class AtomicTest extends IntegrationTestRunner {
+//    @Test
+//    public void testInterleaving1() {
+//        runTest("T1T2");
+//    }
+//
+//    @Test
+//    public void testInterleaving2() {
+//        runTest("T2T1");
+//    }
+//
+//    @Test
+//    public void testInterleaving3() {
+//        runTest("alternateT2T1");
+//    }
+//}
