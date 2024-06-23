a63c31e61c085d9c1ad5370db7a40a5795925873
Ao Li <leeleo3x@gmail.com>
2024-06-22 21:30:08 -0700

    fmt.

--- examples/src/main/kotlin/JUnitRunner.kt
+++ examples/src/main/kotlin/JUnitRunner.kt
@@ -8,30 +8,34 @@ fun main(args: Array<String>) {
   val classAndMethod = args[0].split("#".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
   val request =
       Request.method(
-          Class.forName(classAndMethod[0], true, Thread.currentThread().contextClassLoader),
-          classAndMethod[1],
-      ).filterWith(object : Filter() {
-        var found = false;
-        override fun shouldRun(description: org.junit.runner.Description): Boolean {
-          if (found) return false
-          if (description.methodName == classAndMethod[1]) {
-            found = true
-          }
-          return found
-        }
+              Class.forName(classAndMethod[0], true, Thread.currentThread().contextClassLoader),
+              classAndMethod[1],
+          )
+          .filterWith(
+              object : Filter() {
+                var found = false
 
-        override fun describe(): String {
-          return classAndMethod[1]
-        }
-      })
+                override fun shouldRun(description: org.junit.runner.Description): Boolean {
+                  if (found) return false
+                  if (description.methodName == classAndMethod[1]) {
+                    found = true
+                  }
+                  return found
+                }
+
+                override fun describe(): String {
+                  return classAndMethod[1]
+                }
+              })
 
   val result = JUnitCore().run(request)
   if (!result.wasSuccessful()) {
-    val failureReport = result.failures.joinToString {
-      "testHeader: ${it.testHeader}\n" +
-          "trace: ${it.trace}\n" +
-          "description: ${it.description}\n"
-    }
+    val failureReport =
+        result.failures.joinToString {
+          "testHeader: ${it.testHeader}\n" +
+              "trace: ${it.trace}\n" +
+              "description: ${it.description}\n"
+        }
     throw RuntimeException(failureReport)
   }
 }
