a2e9d2efeec3780358ed66784d847dd17c08968e
Ao Li <leeleo3x@gmail.com>
2024-07-25 17:38:59 -0400

    repo cleanup.

--- benchmarks/script/configs.py
+++
@@ -1,5 +0,0 @@
-import os
-
-BASE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..")
-BENCHMARKS = "fop graphchi h2 h2o jme jython kafka luindex lusearch pmd spring sunflow tomcat tradebeans tradesoap xalan zxing".split(" ")
-GRADLE = os.path.join(BASE, "gradlew")
