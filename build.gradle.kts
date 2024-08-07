6852100b243c8655c5013a36718d45e478de2886
Ao Li <leeleo3x@gmail.com>
2024-08-07 11:43:05 -0400

    update.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
   kotlin("jvm") version "2.0.0"
   id("com.ncorti.ktfmt.gradle") version "0.17.0"
   id("maven-publish")
+  id("org.jetbrains.dokka") version "1.9.20"
 }
 
 
@@ -33,14 +34,34 @@ configure(allprojects - project(":jvmti")) {
 
 configure(allprojects - project("jvmti") - rootProject) {
   plugins.apply("maven-publish")
+  plugins.apply("org.jetbrains.dokka")
+
   afterEvaluate {
+    tasks.register<Jar>("dokkaJavadocJar") {
+      dependsOn(tasks.dokkaJavadoc)
+      from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
+      archiveClassifier.set("javadoc")
+    }
     java {
       withSourcesJar()
     }
     publishing {
       publications {
-        create<MavenPublication>("fray") {
+        create<MavenPublication>("maven") {
+          pom {
+            name = "Fray Testing Framework"
+            description = "Fray testing framework for concurrency programs."
+            url = "github.com/cmu-pasta/fray"
+            licenses {
+              license {
+                name = "GPL-3.0"
+                url = "https://www.gnu.org/licenses/gpl-3.0.html"
+              }
+            }
+
+          }
           from(components["java"])
+          artifact(tasks["dokkaJavadocJar"])
         }
       }
     }
