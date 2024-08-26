96496311a0baef46450f72d5879bd6c92492d957
Ao Li <leeleo3x@gmail.com>
2024-08-26 19:54:58 -0400

    Improve test.

--- core/src/test/java/org/pastalab/fray/core/test/primitives/IntStreamTest.java
+++ core/src/test/java/org/pastalab/fray/core/test/primitives/IntStreamTest.java
@@ -1,18 +1,13 @@
 package org.pastalab.fray.core.test.primitives;
 
 import org.junit.jupiter.api.Test;
-import org.pastalab.fray.core.command.Rand;
-import org.pastalab.fray.core.randomness.ControlledRandom;
 import org.pastalab.fray.core.scheduler.POSScheduler;
 import org.pastalab.fray.core.test.FrayRunner;
 
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IntStreamTest extends FrayRunner {
 
@@ -23,13 +18,9 @@ public class IntStreamTest extends FrayRunner {
             IntStream.range(1, 10).parallel().forEach((i) -> x.compareAndSet(i-1, i+1));
             assert(x.get() != 10);
             return null;
-        }, new POSScheduler(new ControlledRandom(new ArrayList<>(), new ArrayList<>(), new Random())),
+        }, new POSScheduler(),
                 1000000).run();
-        if (result != null) {
-            result.printStackTrace();
-            System.out.println(result);
-        }
-        assertNotNull(result);
+        assertTrue(result instanceof AssertionError);
     }
 
 }
