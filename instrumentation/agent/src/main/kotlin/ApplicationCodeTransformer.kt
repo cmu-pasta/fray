8d7f02090fbc266dd2b978eac4fb49699e8662e9
Ao Li <leeleo3x@gmail.com>
2024-11-18 17:03:41 -0500

    Disable object hashcode instrumenter.

--- instrumentation/agent/src/main/kotlin/ApplicationCodeTransformer.kt
+++ instrumentation/agent/src/main/kotlin/ApplicationCodeTransformer.kt
@@ -72,7 +72,7 @@ class ApplicationCodeTransformer : ClassFileTransformer {
       cv = SleepInstrumenter(cv)
       cv = TimeInstrumenter(cv)
       cv = SkipMethodInstrumenter(cv)
-      cv = ObjectHashCodeInstrumenter(cv, false)
+      //      cv = ObjectHashCodeInstrumenter(cv, false)
       cv = AtomicGetInstrumenter(cv)
       //      cv = ToStringInstrumenter(cv)
       val classVersionInstrumenter = ClassVersionInstrumenter(cv)
