fb0c58f228f3fcbbeb3fc5c02459046d0e397a6b
Ao Li <leeleo3x@gmail.com>
2024-12-20 10:14:44 -0600

    Add more tests for ReentrantReadWriteLock

---
+++ integration-test/src/main/java/org/pastalab/fray/test/success/rwlock/ReentrantReadWriteLockNoDeadlock.java
@@ -0,0 +1,15 @@
+package org.pastalab.fray.test.success.rwlock;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReentrantReadWriteLockNoDeadlock {
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+        lock.readLock().lock();
+        Thread t = new Thread(() -> {
+            lock.readLock().lock();
+        });
+        t.start();
+        t.join();
+    }
+}
