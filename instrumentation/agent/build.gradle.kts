05a10367eb14a7770d6718e75221191b4aa8ef00
Ao Li <leeleo3x@gmail.com>
2025-02-09 10:55:17 -0500

    Generate sourceJar for shadowed modules.

--- instrumentation/agent/build.gradle.kts
+++ instrumentation/agent/build.gradle.kts
@@ -35,6 +35,11 @@ tasks.jar {
   enabled = false
 }
 
+tasks.register<Jar>("sourceJar") {
+  archiveClassifier.set("sources")
+  from(sourceSets.main.get().allSource)
+}
+
 tasks.test {
   useJUnitPlatform()
 }
