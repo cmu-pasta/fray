e36fd505363892a280d50b76e33c14e94ecc9f5e
Ao Li <aoli.al@hotmail.com>
2025-03-22 16:16:26 -0400

    Remove mcp-server folder.

--- mcp-server/src/main/kotlin/org/pastalab/fray/mcp/server/Main.kt
+++
@@ -1,9 +0,0 @@
-package org.pastalab.fray.mcp.server
-
-class Foo {}
-
-fun main() {
-  println(Foo::class.java.protectionDomain.codeSource)
-  val clazz = Foo::class.java
-  Thread.currentThread().stackTrace.forEach { println(it.fileName.toString()) }
-}
