ccf2ed087064904b48dff37eb9bb56e0f0af1bfe
aoli-al <aoli.al@hotmail.com>
2025-06-13 14:30:46 -0400

    Add test to trigger concurrent modification exception.

---
+++ integration-test/src/main/java/org/pastalab/fray/test/core/success/rwlock/ReentrantReadWriteLockMultipleThreadUnlock.java
@@ -0,0 +1,39 @@
+package org.pastalab.fray.test.core.success.rwlock;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReentrantReadWriteLockMultipleThreadUnlock {
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+
+
+
+        for (int i = 0; i < 10; i++) {
+            Thread t1 = new Thread(() -> {
+                rwLock.readLock().lock();
+                atomicInteger.incrementAndGet();
+                rwLock.readLock().unlock();
+            });
+            t1.start();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Thread t1 = new Thread(() -> {
+                rwLock.writeLock().lock();
+                atomicInteger.incrementAndGet();
+                rwLock.writeLock().unlock();
+            });
+            t1.start();
+        }
+
+        rwLock.writeLock().lock();
+        atomicInteger.incrementAndGet();
+        rwLock.writeLock().unlock();
+        rwLock.writeLock().lock();
+        atomicInteger.incrementAndGet();
+        rwLock.writeLock().unlock();
+        rwLock.writeLock().lock();
+    }
+}
