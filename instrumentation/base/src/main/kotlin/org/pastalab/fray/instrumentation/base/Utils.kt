432474151d57dd44cf42a2195927aaa12cdcc1ca
Ao Li <5557706+aoli-al@users.noreply.github.com>
2025-07-04 18:09:10 -0400

    Fix build on macos. (#256)
    * Fix build on macos.
    
    * format

--- instrumentation/base/src/main/kotlin/org/pastalab/fray/instrumentation/base/Utils.kt
+++ instrumentation/base/src/main/kotlin/org/pastalab/fray/instrumentation/base/Utils.kt
@@ -1,34 +1,33 @@
 package org.pastalab.fray.instrumentation.base
 
-import java.io.File
-import java.nio.file.Paths
-import kotlin.io.path.ExperimentalPathApi
+import java.nio.file.Files
+import java.nio.file.Path
 import kotlin.io.path.createDirectories
-import kotlin.io.path.deleteRecursively
+import kotlin.io.path.writeBytes
 import org.pastalab.fray.instrumentation.base.Configs.DEBUG_MODE
 
 object Utils {
-  @OptIn(ExperimentalPathApi::class)
-  fun prepareDebugFolder(folder: String) {
-    val path = Paths.get("/tmp/out/$folder")
-    path.deleteRecursively()
-    path.createDirectories()
-  }
+  val ORIGIN_CLASS_LOCATION: Path
+  val INSTRUMENTED_CLASS_LOCATION: Path
 
   fun writeClassFile(className: String, classBytes: ByteArray, instrumented: Boolean) {
     if (instrumented) {
-      File("/tmp/out/instrumented/${className.replace("/", ".").removePrefix(".")}.class")
+      INSTRUMENTED_CLASS_LOCATION.resolve(className.replace("/", ".").removePrefix(".") + ".class")
           .writeBytes(classBytes)
     } else {
-      File("/tmp/out/origin/${className.replace("/", ".").removePrefix(".")}.class")
+      ORIGIN_CLASS_LOCATION.resolve(className.replace("/", ".").removePrefix(".") + ".class")
           .writeBytes(classBytes)
     }
   }
 
   init {
+    val location = Files.createTempDirectory("fray-instrumentation")
+    ORIGIN_CLASS_LOCATION = location.resolve("origin")
+    ORIGIN_CLASS_LOCATION.createDirectories()
+    INSTRUMENTED_CLASS_LOCATION = location.resolve("instrumented")
+    INSTRUMENTED_CLASS_LOCATION.createDirectories()
     if (DEBUG_MODE) {
-      prepareDebugFolder("instrumented")
-      prepareDebugFolder("origin")
+      println("Debug mode enabled, classes will be written to: $location")
     }
   }
 }
